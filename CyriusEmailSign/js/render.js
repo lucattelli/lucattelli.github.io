56e7fd913569d3d06b217d1c14912e54c9d2eb29
Bruno Lucattelli <Bruno Lucattelli>
2016-11-01 15:54:11 -0200

    Fix images

--- CyriusEmailSign/js/render.js
+++ CyriusEmailSign/js/render.js
@@ -22,11 +22,21 @@ function generate() {
 	
 }
 
+function downloadURI(uri, name) {
+	var link = document.createElement("a");
+	link.download = name;
+	link.href = uri;
+	document.body.appendChild(link);
+	link.click();
+	document.body.removeChild(link);
+	delete link;
+}
+
 function asimage() {
 	html2canvas($("#sign"), {
 	    onrendered: function(canvas) {
 	        var img = canvas.toDataURL();
-	        $("#wrapper").html('<img src="'+img+'"/>');
+	        downloadURI(img, "sign.png");
 	    }
 	});
 }
